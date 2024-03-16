82e0690b5f18dd637a99bc26ef7e36a57ed8b792
EKL-NCI <114241384+EKL-NCI@users.noreply.github.com>
2024-03-16 22:52:32 +0000

    .

--- src/musicapplication/playlistNode.java
+++ src/musicapplication/playlistNode.java
@@ -8,39 +8,66 @@ package musicapplication;
  *
  * @author erink
  */
-public class playlistNode {
+public class PlaylistNode {
     private Song song;
-    private playlistNode next;
-    private playlistNode prev;
-    
-    public playlistNode(Song inSong, playlistNode inNext, playlistNode inPrev){
-        this.song = inSong;
-        this.next = inNext;
-        this.prev = inPrev;
+    private PlaylistNode next;
+    private PlaylistNode prev;
+
+    public PlaylistNode(Song song) {
+        this.song = song;
+        this.next = null;
+        this.prev = null;
     }
 
-    public playlistNode getNext() {
+    public PlaylistNode getNext() {
         return next;
     }
 
-    public void setNext(playlistNode inNext) {
-        next = inNext;
+    public void setNext(PlaylistNode next) {
+        this.next = next;
     }
 
-    public playlistNode getPrev() {
+    public PlaylistNode getPrev() {
         return prev;
     }
 
-    public void setPrev(playlistNode inPrev) {
-        prev = inPrev;
+    public void setPrev(PlaylistNode prev) {
+        this.prev = prev;
     }
-    
+
     public Song getSong() {
         return song;
     }
 
-    public void setSong(Song inSong){
-        song = inSong;
+    public void setSong(Song song){
+        this.song = song;
     }
-    
+
+    protected void linkNext(PlaylistNode node) {
+        this.next = node;
+        if (node != null) {
+            node.prev = this;
+        }
+    }
+
+    protected void linkPrev(PlaylistNode node) {
+        this.prev = node;
+        if (node != null) {
+            node.next = this;
+        }
+    }
+
+    protected void unlinkNext() {
+        if (this.next != null) {
+            this.next.prev = null;
+            this.next = null;
+        }
+    }
+
+    protected void unlinkPrev() {
+        if (this.prev != null) {
+            this.prev.next = null;
+            this.prev = null;
+        }
+    }
 }
